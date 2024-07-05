7bc67faa8640a05a6fb98f58d34f3813f2bce843
Juli <luigfmp@gmail.com>
2024-07-05 01:57:44 -0500

    add speed insights

--- src/app/Providers.tsx
+++ src/app/Providers.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { ClerkProvider } from '@clerk/nextjs'
+import { SpeedInsights } from '@vercel/speed-insights/next'
 import { ViewTransitions } from 'next-view-transitions'
 import type { JSX, ReactNode } from 'react'
 
@@ -13,6 +14,7 @@ const Providers = ({ children }: IProviders): JSX.Element => {
     <ClerkProvider>
       <ViewTransitions>
         <html lang='es'>{children}</html>
+        <SpeedInsights />
       </ViewTransitions>
     </ClerkProvider>
   )
