5dfa5037955d7680df49322e859933a83eec18d9
Luis fernando JU <luigfmp@gmail.com>
2024-09-05 23:35:49 -0500

    ci: :green_heart: fix problems

--- src/app/api/reservation/by-user/[idUser]/route.ts
+++ src/app/api/reservation/by-user/[idUser]/route.ts
@@ -5,23 +5,27 @@ import { NextRequest, NextResponse } from 'next/server'
 export type TClientReservation = Prisma.ReservationGetPayload<{
   include: { room: { include: { images: true } }; client: true }
 }>
+
 interface IGetReservationParams {
   params: { idUser: string }
 }
+
 export async function GET(req: NextRequest, { params }: IGetReservationParams) {
   try {
     const { idUser } = params
     const clientData = await prisma.client.findUnique({
       where: { clerkId: idUser ?? '' }
     })
-    if (!clientData) throw new Error("The client account don't exist")
+    if (!clientData) throw new Error("The client account doesn't exist")
+
     const reservations = await prisma.reservation.findMany({
       where: { clientId: clientData.clientId },
       include: { room: { include: { images: true } }, client: true }
     })
-    return Response.json(reservations)
+
+    return NextResponse.json(reservations)
   } catch (error: any) {
-    console.log(error)
-    return new NextResponse(error?.message ?? 'INTERNAL SERVER ERROR', { status: 501 })
+    console.error(error)
+    return new NextResponse(error?.message ?? 'INTERNAL SERVER ERROR', { status: 500 })
   }
 }
