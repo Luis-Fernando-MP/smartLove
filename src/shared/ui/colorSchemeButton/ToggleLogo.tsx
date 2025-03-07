436d713891fe3ce447f37addf42c0c8a078fc006
Juls <luigfmp@gmail.com>
2025-03-07 01:29:31 -0500

    refactor: 🎨 Update ToggleLogo component styling and typography

--- src/shared/ui/colorSchemeButton/ToggleLogo.tsx
+++ src/shared/ui/colorSchemeButton/ToggleLogo.tsx
@@ -9,12 +9,10 @@ interface IToggleLogo {
 
 const ToggleLogo = ({ className = 'logo' }: IToggleLogo): JSX.Element => {
   console.log('ToggleLogo', className)
-  // const theme = useStoreTheme(store => store.theme)
-  // if (theme === 'dark') return <Logo className={className} />
   return (
     <div className={`${sansitaSwashed.className} logo-container`}>
-      <h4>Smart</h4>
-      <h2 className='gr'>PRO</h2>
+      <h3 className='font3'>Smart</h3>
+      <h2 className='gr font3'>PRO</h2>
     </div>
   )
 }
